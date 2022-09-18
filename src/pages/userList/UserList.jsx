6405536a89e930ff7a94bbbf4f8ec7e8f159eaa9
Akshay Gore <akshaygore2301@gmail.com>
2022-09-18 17:10:22 +0530

    userlist fixes intial changes

--- src/pages/userList/UserList.jsx
+++ src/pages/userList/UserList.jsx
@@ -3,19 +3,44 @@ import { DataGrid } from "@material-ui/data-grid";
 import { DeleteOutline } from "@material-ui/icons";
 import { userRows } from "../../dummyData";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { userRequest } from "../../requestMethods";
 
 export default function UserList() {
-  const [data, setData] = useState(userRows);
+  // const [data, setData] = useState(userRows);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const res = await userRequest.get("users/");
+        let data = res.data;
+        let rowData = [];
+        if (data.length) {
+          for (let i = 0; i < data.length; i++) {
+            let obj = {};
+            obj.id = data[i]._id;
+            obj.email = data[i].email;
+            obj.username = data[i].username;
+            rowData.push(obj);
+          }
+        }
+        setUsers(rowData);
+      } catch (err) {
+        console.log("Oops Something Wen Wrong");
+      }
+    };
+    getUsers();
+  }, []);
 
   const handleDelete = (id) => {
-    setData(data.filter((item) => item.id !== id));
+    setUsers(users.filter((item) => item.id !== id));
   };
-  
+
   const columns = [
     { field: "id", headerName: "ID", width: 90 },
     {
-      field: "user",
+      field: "username",
       headerName: "User",
       width: 200,
       renderCell: (params) => {
@@ -28,16 +53,7 @@ export default function UserList() {
       },
     },
     { field: "email", headerName: "Email", width: 200 },
-    {
-      field: "status",
-      headerName: "Status",
-      width: 120,
-    },
-    {
-      field: "transaction",
-      headerName: "Transaction Volume",
-      width: 160,
-    },
+
     {
       field: "action",
       headerName: "Action",
@@ -61,7 +77,7 @@ export default function UserList() {
   return (
     <div className="userList">
       <DataGrid
-        rows={data}
+        rows={users}
         disableSelectionOnClick
         columns={columns}
         pageSize={8}
