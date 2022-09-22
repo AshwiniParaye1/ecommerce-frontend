e48e1be46b767f4cdd53071863c83b25ec378d06
Akshay Gore <akshaygore2301@gmail.com>
2022-09-22 22:58:49 +0530

    fire base integration

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,41 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAqDcv-YAxHwIACHEFu_Ta4gZwpoiR31D8",
+//   authDomain: "shop-6953e.firebaseapp.com",
+//   projectId: "shop-6953e",
+//   storageBucket: "shop-6953e.appspot.com",
+//   messagingSenderId: "545750438409",
+//   appId: "1:545750438409:web:29f2f8d18df1ec22ef5ed8",
+//   measurementId: "G-1HY7XMMC01",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// export default app;
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCB-h_T2ZL-a7xt5zMvZmSh8GBXtawzL0c",
-  authDomain: "shop-889c6.firebaseapp.com",
-  projectId: "shop-889c6",
-  storageBucket: "shop-889c6.appspot.com",
-  messagingSenderId: "105110075028",
-  appId: "1:105110075028:web:7cee5adf1ed39f597e6681",
+  apiKey: "AIzaSyAqDcv-YAxHwIACHEFu_Ta4gZwpoiR31D8",
+  authDomain: "shop-6953e.firebaseapp.com",
+  projectId: "shop-6953e",
+  storageBucket: "shop-6953e.appspot.com",
+  messagingSenderId: "545750438409",
+  appId: "1:545750438409:web:29f2f8d18df1ec22ef5ed8",
+  measurementId: "G-1HY7XMMC01",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export default app;
